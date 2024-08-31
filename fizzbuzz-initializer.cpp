2cd992d31c09de2bbd0ebfaef48d15bed7444261
AlexLoz0304 <alozano0304@utexas.edu>
2024-08-31 15:26:36 -0500

    hw 1

---
+++ fizzbuzz-initializer.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+float read_number(){
+  float i;
+  cin >> i;
+  return i;
+}
+
+int main() {
+
+  if ( int i = read_number(); i % 3 == 0 and i % 5 == 0 )
+    cout << "FizzBuzz" << "\n";
+  else if ( i % 3 == 0 )
+    cout << "Fizz" << "\n";
+  else if ( i % 5 == 0 )
+    cout << "Buzz" << "\n";
+ 
+  return 0;
+}
