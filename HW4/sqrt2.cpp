34eedeb6f7f9e6110182744eed548014d7ad51ef
alozano0304 <alozano0304@isp03.tacc.utexas.edu>
2024-09-05 16:28:44 -0500

    HW4

---
+++ HW4/sqrt2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+double fx(double x){
+
+  return (x * x) - 2;
+}
+
+double deriv(double x){
+
+  return 2 * x;
+}
+
+
+int main() {
+  
+  double  x = 2;
+  double funcval = fx(x);
+  int i = 0;
+  while (abs(funcval) > 1.e-8 && i < 100) {
+
+    x = x - (fx(x)/deriv(x));
+    funcval = fx(x);
+    cout << "Funciton is "<< x << "at " << fx(x) << "\n";
+    i ++;
+  }
+  cout << "It took " << i << " iterations" << "\n";
+  return 0;
+}
