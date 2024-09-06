5d00969e8b19bf3a7f94948cc7682027f559371f
AlexLoz0304 <alozano0304@utexas.edu>
2024-09-06 18:36:15 -0500

    HW3

---
+++ HW3/collatz.cpp
@@ -0,0 +1,34 @@
+//Alexander Lozano
+//AJL4846
+//alozano0304
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int counter = 0;
+    int x;
+    int j;
+
+    for (int i = 1; i <= 1e6; i++) {
+
+        x = i;
+        j = 0;
+
+        while (x != 1){
+            if (x % 2 == 0) {
+                x = x / 2;
+            } else {
+                x = 3 * x + 1;
+            }
+            j ++;
+        }
+
+        if (j > counter){
+            counter = j;
+            cout << i << " is the new longest sequence with " << j << " iterations" << "\n";
+        }
+
+    }
+    return 0;
+}
---
+++ temp.cpp
@@ -0,0 +1,11 @@
+//Alexander Lozano
+//AJL4846
+//alozano0304
+#include <iostream>
+using namespace std;
+
+int main () {
+
+
+    return 0
+}
