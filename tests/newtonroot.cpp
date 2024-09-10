3ae8a9df7ccb3e78f4a4abecc5052fe0941e6869
AlexLoz0304 <alozano0304@utexas.edu>
2024-09-10 16:09:11 -0500

    Create newtonroot.cpp

---
+++ tests/newtonroot.cpp
@@ -0,0 +1,36 @@
+//Alexander Lozano
+//AJL4846
+//alozano0304
+#include <iostream>
+#include <functional>
+using namespace std;
+
+double newton_root ( function< double(double) > f,
+function< double(double) > fprime ) {
+  
+  double  x = 100;
+  double funcval = fprime(x);
+  int i = 0;
+
+  while (abs(funcval) > 1.e-8 && i < 100) {
+
+    x = x - (f(x)/fprime(x));
+    funcval = f(x);
+    cout << "Funciton is "<< x << "at " << f(x) << "\n";
+    i ++;
+  }
+  cout << "It took " << i << " iterations" << "\n";
+}
+
+int main() {
+
+  auto f = [] (double x) { return (x * x) - 2;};
+  auto fprime = [] (double x) { return 2 * x;};
+
+  double root = newton_root( f,fprime );
+  
+  return 0;
+}
+
+
+
