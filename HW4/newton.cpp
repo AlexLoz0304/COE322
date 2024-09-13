ac732f3625f7dd620ab4283bc281da99c07092cc
alozano0304 <alozano0304@isp03.tacc.utexas.edu>
2024-09-12 19:44:21 -0500

    update

--- HW4/newton.cpp
+++ HW4/newton.cpp
@@ -7,19 +7,9 @@ using namespace std;
 
 double newton_root ( function< double(double) > f ) {
   
-  auto fprime  = [] (function< double(double) > f, double x) { return (f(x + 1e-6) - f(x))/1e-6;};
+  auto fprime  = [f] ( double x) { return (f(x + 1e-6) - f(x))/1e-6;};
 
-  double  x = 1000;
-  double funcval = fprime(f,x);
-  int i = 0;
-
-  while (abs(funcval) > 1.e-8 && i < 100) {
-
-    x = x - (f(x)/fprime(f,x));
-    funcval = f(x);
-    i ++;
-  }
-  return x;
+  return newton_root(f,fprime);
  }
 
 double newton_root ( function< double(double) > f, function< double(double) > fprime ) {
