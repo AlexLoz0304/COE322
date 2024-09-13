e0aeac172c3eaa597b5f4c293b6cae635b77bfe6
AlexLoz0304 <alozano0304@utexas.edu>
2024-09-12 19:34:17 -0500

    Update newton.cpp

--- HW4/newton.cpp
+++ HW4/newton.cpp
@@ -9,17 +9,8 @@ double newton_root ( function< double(double) > f ) {
   
   auto fprime  = [] (function< double(double) > f, double x) { return (f(x + 1e-6) - f(x))/1e-6;};
 
-  double  x = 1000;
-  double funcval = fprime(f,x);
-  int i = 0;
-
-  while (abs(funcval) > 1.e-8 && i < 100) {
-
-    x = x - (f(x)/fprime(f,x));
-    funcval = f(x);
-    i ++;
-  }
-  return x;
+  double root = newton_root(f,fprime);
+  return root
  }
 
 double newton_root ( function< double(double) > f, function< double(double) > fprime ) {
@@ -41,21 +32,23 @@ double newton_root ( function< double(double) > f, function< double(double) > fp
 
 int main() {
 
-    double n;
-    cout << "Please enter a number you'd like to take a square root of" << "\n";
-    cin >> n;
-
-  auto f = [n] (double x) { return (x * x) - n;};
-
-  double root = newton_root( f );
-  cout << "The root of the number is " << root << "\n";
-
   for (int n=2; n <=8; ++n) {
     auto f = [n] (double x) { return (x * x) - n;};
     auto fprime = [] (double x) { return 2 * x;};
     cout << "sqrt(" << n << ") = " << newton_root(f,fprime) << "\n";
 
   }
+
+  double n;
+  cout << "Please enter a number you'd like to take a square root of" << "\n";
+  cin >> n;
+
+  auto f = [n] (double x) { return (x * x) - n;};
+
+  double root = newton_root( f );
+  cout << "The root of the number is " << root << "\n";
+
+  
   
   return 0;
 }
