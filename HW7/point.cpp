37b9746ed17c7f8f7fedd28c549654e1acd6c4c6
alozano0304 <alozano0304@isp03.tacc.utexas.edu>
2024-10-03 19:41:27 -0500

    update

--- HW7/point.cpp
+++ HW7/point.cpp
@@ -10,7 +10,6 @@ class Point {
 private:
   float x,y;
 public:
-  //constructor
   Point(float in_x,float in_y)
     : x(in_x),y(in_y) {}
   float distance_to_origin(){
@@ -19,7 +18,6 @@ public:
   float distance(Point q){
     return sqrt(pow((x-q.x),2) + pow((y-q.y),2));
   };
-  //method
   float angle(){
     return atan(y/x);
   };
@@ -43,18 +41,7 @@ int main () {
   p.asstring();
   Point p1(3.0,4.0);
   p1.asstring();
-  float d = p1.distance_to_origin();
-  float a = p1.angle();
-  cout << d << "  " << a << "\n";
-  float dist = p.distance(p1);
-  cout << dist << "\n";
   Point h = p.halfway(p1);
   h.asstring();
-  Point n = p + p1;
-  n.asstring();
-  Point p2 = n * 5;
-  p2.asstring();
-  Point p3 = p1.halfway(p2);
-  p3.asstring();
-    return 0;
+  return 0;
 }
