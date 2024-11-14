d92aa4016d1f3505cd9cd758323a90d7cedab1b4
alozano0304 <alozano0304@isp03.tacc.utexas.edu>
2024-11-14 16:01:09 -0600

    update

---
+++ HW12build/CMakeFiles/abctuple.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/abctuple.dir/abctuple.cpp.o"
+  "CMakeFiles/abctuple.dir/abctuple.cpp.o.d"
+  "abctuple"
+  "abctuple.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/abctuple.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ HW12build/CMakeFiles/abctuple.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for abctuple.
+# This may be replaced when dependencies are built.
